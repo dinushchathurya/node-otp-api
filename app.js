00d62c6186ee62edf58064922f9615bbf1613360
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-06 13:34:21 +0530

    feat: import all required pacakages to app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer')
+const path = require('path')
+const exhbs = require('express-handlebars')
