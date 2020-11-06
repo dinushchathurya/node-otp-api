d1a5a50e8d1683de8804b96f33ce22d1489369e0
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-06 15:13:03 +0530

    feat: setup handlebar  view engine

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ const path = require('path')
 const exhbs = require('express-handlebars')
 
 const app = express();
+
+//View Engine
+app.engine('handlebard', exphbs({ extname: "hbs", defaultLayout: false, layoutsDir: "views/ " }));
+app.set('view engine', 'handlebars');
