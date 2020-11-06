502b6e5fb36b46c1174d810bed77e4e12956c5e6
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-06 20:23:14 +0530

    feat: create mail sending, otp verification, mail resend functions and nodemailer configuration

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express')
 const bodyparser = require('body-parser')
 const nodemailer = require('nodemailer')
 const path = require('path')
-const exhbs = require('express-handlebars')
+const exphbs = require('express-handlebars')
 
 const app = express();
 
 //View Engine
-app.engine('handlebars', exhbs({ extname: "hbs", defaultLayout: false, layoutsDir: "views/ " }));
+app.engine('handlebars', exphbs({ extname: "hbs", defaultLayout: false, layoutsDir: "views/ " }));
 app.set('view engine', 'handlebars');
 
 // body parser middleware
@@ -16,8 +16,78 @@ app.use(bodyparser.json());
 
 //static folder
 app.use('/public', express.static(path.join(__dirname, 'public')));
+
 app.get('/', function (req, res) {
-    res.send("Node.js 2 way authnetication");
+    res.render('contact');
+});
+
+var email;
+
+var otp = Math.random();
+otp = otp * 1000000;
+otp = parseInt(otp);
+console.log(otp);
+
+let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    service: 'Gmail',
+
+    auth: {
+        user: 'Your E-Mail',
+        pass: 'Your Password',
+    }
+
+});
+
+app.post('/send', function (req, res) {
+    email = req.body.email;
+
+    // send mail with defined transport object
+    var mailOptions = {
+        to: req.body.email,
+        subject: "Otp for registration is: ",
+        html: "<h3>OTP for account verification is </h3>" + "<h1 style='font-weight:bold;'>" + otp + "</h1>" // html body
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        res.render('otp');
+    });
+});
+
+app.post('/verify', function (req, res) {
+
+    if (req.body.otp == otp) {
+        res.send("You has been successfully registered");
+    }
+    else {
+        res.render('otp', { msg: 'otp is incorrect' });
+    }
+});
+
+app.post('/resend', function (req, res) {
+    var mailOptions = {
+        to: email,
+        subject: "Otp for registration is: ",
+        html: "<h3>OTP for account verification is </h3>" + "<h1 style='font-weight:bold;'>" + otp + "</h1>" // html body
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        res.render('otp', { msg: "otp has been sent" });
+    });
+
 });
 
 //defining port
